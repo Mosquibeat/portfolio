13ce3c0cbc04cc239bc16f1c0a706f493d6310b3
Marc-Olivier Castagnetti <marcolivier.castagnetti@gmail.com>
2016-03-29 16:50:05 +0200

    amelio

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ import {About} from './components/about/about';
   styles: [require('assets/scss/style.scss')]
 })
 @RouteConfig([
-  { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
+  { path: '/',           component: Home,        name: 'Home', useAsDefault: true },
   { path: '/about',      component: About,       name: 'About' },
 ])
 export class App {
